293b3d4729b8d8ad7ccfbaa1dcc77b7803d9af2d
Joe Kutner <jpkutner@gmail.com>
2019-08-26 21:54:22 -0500

    circleci install bundler

---
+++ circleci-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -eu
+
+JRUBY_VERSION="${1:?}"
+CLEAR_TEXT_CIRCLECI_TOKEN="$(echo "${CIRCLECI_TOKEN:?}" | gpg --decrypt)"
+
+curl -X POST \
+  --header "Content-Type: application/json" \
+  -d "{\"name\":\"JRUBY_VERSION\", \"value\":\"${JRUBY_VERSION}\"}" \
+  "https://circleci.com/api/v1.1/project/github/heroku/docker-heroku-jruby-builder/envvar?circle-token=${CLEAR_TEXT_CIRCLECI_TOKEN}"
+
+curl -X POST \
+  --header "Content-Type: application/json" \
+  -d '{"branch": "master"}' \
+  "https://circleci.com/api/v1.1/project/github/heroku/docker-heroku-jruby-builder/build?circle-token=${CLEAR_TEXT_CIRCLECI_TOKEN}"
