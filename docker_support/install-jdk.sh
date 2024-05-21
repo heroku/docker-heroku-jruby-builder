db4c775367576026d505ec4922f6b20146ed7577
Schneems <richard.schneeman+foo@gmail.com>
2024-05-21 12:16:07 -0500

    Add Heroku-24 and ARM support
    - Rewrites the project to use a pre-built JRuby binary. Links to downloads are at https://www.jruby.org/download.
    - Adds the heroku-24 base image as a build target
    - Re-uses the ARM url format for S3 that was introduced in https://github.com/heroku/docker-heroku-ruby-builder/pull/38. Both the Ruby classic buildpack and CNB are tightly coupled to this format and will attempt to download binaries from S3 with cpu architecture information on heroku-24. Once built and uploaded to S3 there should be no changes needed to the buildpack to acquire support for heroku-24 and ARM due to:
      - https://github.com/heroku/heroku-buildpack-ruby/pull/1439
      - https://github.com/heroku/buildpacks-ruby/pull/284

--- docker_support/install-jdk.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /opt/jvm-common
-curl --silent --location https://buildpack-registry.s3.amazonaws.com/buildpacks/heroku/jvm.tgz | tar xzm -C /opt/jvm-common --strip-components=1
-source /opt/jvm-common/bin/util
-source /opt/jvm-common/bin/java
-mkdir -p /opt/jdk
-install_java /opt/jdk $1
